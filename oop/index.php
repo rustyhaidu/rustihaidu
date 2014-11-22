2bf4976d53f6494ec94bff9f423822ca88377c0e
Negreanu Calin <negreanu0calin@gmail.com>
2014-11-22 14:47:04 +0200

    [add]

---
+++ oop/index.php
@@ -0,0 +1,115 @@
+<?php
+// ->method()   [dot equivalent Java]
+// :: static
+
+abstract class Autovehicle {
+    protected $wheels = 2;
+}
+
+class Car extends Autovehicle {
+    protected $wheels;    //suprascri
+    private $doors = 4;       //nu mosteneste copii, poate fi apelata din interior ($car->doors //fatala)
+    public $color = 'red';    //se mosteneste si poate fi accesat direct $car->color;
+    protected $name ='aa';    //se mosteneste dar nu poate fi accesat direct
+    static $smth ='smth val'; //poate fi apelat static  Car::smth
+    private $passengers = array();
+
+    public function __construct() {
+        $this->wheels = 4;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getPassengerIds() {
+        $ids = array();
+        //if(!empty($this->passengers)) {
+            foreach($this->passengers as $pass) {
+                $ids[]=$pass->id;
+            }
+        //}
+        return $ids;
+    }
+
+    static function checkVariables($car) {
+        $name = $car->getName();
+        if(empty($name)){
+            return false;
+        }
+    }
+
+    private function getDoors(){
+        return $this->doors;
+    }
+}
+
+class Ww extends Car{
+    private $doors = 2;
+
+    private function getDoors() {
+        return $this->doors;
+    }
+
+    public function showDoors() {
+        echo '<br>Doors :'.$this->getDoors();
+    }
+}
+
+final class Beattle extends Car{
+    private $doors = 3;
+
+    private function getDoors() {
+        return $this->doors;
+    }
+
+    public function showDoors() {
+        echo '<br>Doors :'.$this->getDoors();
+    }
+}
+
+//FATAL
+/**
+class B1 extends Beattle
+{
+
+}
+**/
+
+echo Car::$smth;
+$car = new Car();
+echo $car->color;
+
+Car::checkVariables($car);
+
+$wwObj = new Ww();
+echo '<br>'.$wwObj->showDoors();
+$wwObj = new Beattle();
+echo '<br>'.$wwObj->showDoors();
+
+class DateGenerator {
+//............(existing methods)...........//
+    static function getMonth($month) {
+        switch($month){
+            case 1:
+                return 'January';
+                break;
+        }
+    }
+
+    public function getMonth2($month) {
+        switch($month){
+            case 1:
+                return 'January';
+                break;
+    }
+}
+}
+
+$dateGeneratorObject = new DateGenerator();
+$dateGeneratorObject->getMonth2(1);
+//  OR Better !!!!
+DateGenerator::getMonth(1);
+
+
+
