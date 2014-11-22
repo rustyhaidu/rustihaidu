883d988e2537176bf85170f2c304e87a0e973dba
Negreanu Calin <negreanu0calin@gmail.com>
2014-11-22 13:16:45 +0200

    [FIX] - Fucntions

--- tema/functions_views.php
+++ tema/functions_views.php
@@ -1,36 +1,51 @@
 <?php
-function show($loggedIn)
-{
-	if(isset($_GET['message']))
-{
-view_show_message($_GET['message']);
+function get_message($messageNumber) {
+    switch($messageNumber) {
+        case 1:
+            $message = 'User is locked';
+            break;
+    }
+    return $message;
 }
-if ($loggedIn == 0)
+
+function view_show_message($errorMessage) {
+    $message = get_message($errorMessage);
+    echo $message;
+}
+
+function show($user)
 {
-echo '
-<form id="form1" name="form1" method="post" action="index.php">
-<table width="510" border="0" align="center">
-<tr>
-<td>Username</td>
-<td><input type="text" name="username" id="username" /></td>
-</tr>
-<tr>
-<td>Password</td>
-<td><input type="password" name="password" id="password" /></td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td><input type="submit" name="button_login" id="button" value="Login" />
-<a href="register.php">Register</a>
-</td>
-</tr>
-</table>
-</form>';
+	if (isset($_GET['message'])) {
+        view_show_message($_GET['message']);
+    }
+    if ($user) {
+        echo '<table width="510" border="0" align="center"> <td>Welcome ' .$user['username']."</td></table>";
+        echo '<form id="form1" name="form1" method="post" action="logout.php">
+            <table width="510" border="0" align="center">
+            <td><input type="submit" name="button" id="button" value="logout" /></td>
+            </table>
+            </form>';
+        echo "<a href='logout.php'>Logout</a>";
+    } else {
+        echo '
+        <form id="form1" name="form1" method="post" action="index.php">
+        <table width="510" border="0" align="center">
+        <tr>
+        <td>Username</td>
+        <td><input type="text" name="username" id="username" /></td>
+        </tr>
+        <tr>
+        <td>Password</td>
+        <td><input type="password" name="password" id="password" /></td>
+        </tr>
+        <tr>
+        <td>&nbsp;</td>
+        <td><input type="submit" name="button_login" id="button" value="Login" />
+        <a href="register.php">Register</a>
+        </td>
+        </tr>
+        </table>
+        </form>';
+    }
 }
-else
-	{
-		$newURL = 'logout.php';
-		header('Location: '.$newURL);
-		die();
-	}
-}
+
--- tema/index.php
+++ tema/index.php
@@ -4,8 +4,10 @@ require 'functions.php';
 require 'functions_views.php';
 $con = connect();
 check_empty();
-check_credentials($con);
-unlock_users();
-$loggedIn = logged();
-show($loggedIn);
-?>
+$user = FALSE;
+if (isset($_POST['username']) && (!empty($_POST['username'])) && isset($_POST['password']) && (!empty($_POST['password'])) ) {
+    $user = check_credentials($con);
+}
+unlock_users($con);
+$user = logged($con);
+show($user);
