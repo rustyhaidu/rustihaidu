f3ffd5d6a753aebd11bcbd67d7a5d8c176178e75
rustyhaidu <rustyhaidu@yahoo.com>
2015-01-25 13:01:18 +0200

    list events

--- Array51/calendar/database_functions.php
+++ Array51/calendar/database_functions.php
@@ -20,3 +20,23 @@ function insert_event($day,$month,$year, $title, $detail)
     $sqlinsert = "insert into events (id,Title,Details,EventDate) values ('','".$title."','".$detail."','".$eventdate."')";
     return mysqli_query($connection,$sqlinsert);
 }
+
+function show_events($day,$month,$year)
+{
+global $connection;
+$query = 'Select title, details, eventdate,addedevent from events where eventdate = "'.$day.'/'.$month.'/'.$year.'"';
+			$result = mysqli_query($connection, $query);
+			echo '<table border="1">';
+			foreach ($result as $value)
+			{
+				echo '<tr>
+							<td>'.$value['title'].'</td>
+							<td>'.$value['details'].'</td>
+							<td>'.$value['eventdate'].'</td>
+							<td>'.$value['addedevent'].'</td>
+							<td><input style="height:50px" type="submit" type ="button" value="EDIT" name ="edit">
+								</td>
+				     </tr>';
+			}
+			echo '</table>';
+}		
