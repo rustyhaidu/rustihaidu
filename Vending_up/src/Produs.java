4125d26025fe26451097ea76028fc378dab57f33
rustyhaidu <rustyhaidu@yahoo.com>
2015-02-27 17:26:54 +0200

    java vending

---
+++ Vending_up/src/Produs.java
@@ -0,0 +1,19 @@
+
+public class Produs {
+public String nume;
+public int price;
+public int id;
+
+
+
+	public Produs(String nume,int price,int id) 
+	{
+		this.nume = nume;
+		this.price = price;
+		this.id = id;		
+	}
+	
+
+	
+	
+}
